238a745cafffda9fc1d9eb82bc4504bf204c12a1
AnilBurcu <anlbrc95@gmail.com>
2024-04-08 16:21:56 +0300

    Add Page

--- src/app/slices/jobSlice.js
+++ src/app/slices/jobSlice.js
@@ -1,32 +1,36 @@
-import { createSlice } from '@reduxjs/toolkit'
-
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     jobs: [],
     isLoading: false,
     error: null,
-}
-
+};
 
 const jobSlice = createSlice({
-    name: "job",
-    initialState: initialState,
+    name: 'job',
+    initialState,
     reducers: {
         setLoading: (state) => {
-            state.isLoading = true
+            state.isLoading = true;
         },
+
         setError: (state, action) => {
-            state.isLoading = false,
-                state.error = action.payload
+            state.isLoading = false;
+            state.error = action.payload;
         },
+
         setJobs: (state, action) => {
-            state.isLoading = false,
-                state.error = null,
-                state.jobs = action.payload
-        }
-    }
+            state.isLoading = false;
+            state.error = null;
+            state.jobs = action.payload;
+        },
 
-})
-export default jobSlice.reducer
+        createJob: (state, action) => {
+            state.jobs.push(action.payload);
+        },
+    },
+});
 
-export const { setJobs, setError, setLoading } = jobSlice.actions
+export default jobSlice.reducer;
+
+export const { setJobs, setError, setLoading, createJob } = jobSlice.actions;
